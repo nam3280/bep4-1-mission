0900d987781118118d2ab71391ea5a4dc1975a27
nam3280 <nam3280@naver.com>
2025-12-26 11:10:52 +0900

    0024

--- src/main/java/com/back/boundedContext/cash/in/CashEventListener.java
+++ src/main/java/com/back/boundedContext/cash/in/CashEventListener.java
@@ -2,6 +2,7 @@ package com.back.boundedContext.cash.in;
 
 
 import com.back.boundedContext.cash.app.CashFacade;
+import com.back.boundedContext.cash.domain.CashMember;
 import com.back.shared.member.event.MemberJoinedEvent;
 import com.back.shared.member.event.MemberModifiedEvent;
 import lombok.RequiredArgsConstructor;
@@ -21,6 +22,9 @@ public class CashEventListener {
     @Transactional(propagation = REQUIRES_NEW)
     public void handle(MemberJoinedEvent event) {
         cashFacade.syncMember(event.getMember());
+        CashMember member = cashFacade.syncMember(event.getMember());
+
+        cashFacade.createWallet(member);
     }
 
     @TransactionalEventListener(phase = AFTER_COMMIT)
