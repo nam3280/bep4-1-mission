011eab6830707ec9533f34dfdfa9fb91abc5da6c
nam3280 <nam3280@naver.com>
2025-12-30 12:21:37 +0900

    0042

--- src/main/java/com/back/boundedContext/payout/app/PayoutCreatePayoutUseCase.java
+++ src/main/java/com/back/boundedContext/payout/app/PayoutCreatePayoutUseCase.java
@@ -1,13 +1,22 @@
 package com.back.boundedContext.payout.app;
 
+import com.back.boundedContext.payout.domain.Payout;
+import com.back.boundedContext.payout.domain.PayoutMember;
+import com.back.boundedContext.payout.out.PayoutMemberRepository;
+import com.back.boundedContext.payout.out.PayoutRepository;
 import com.back.shared.payout.dto.PayoutMemberDto;
-import lombok.extern.slf4j.Slf4j;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-@Slf4j
 @Service
+@RequiredArgsConstructor
 public class PayoutCreatePayoutUseCase {
-    public void createPayout(PayoutMemberDto payee) {
-        log.debug("createPayout.payee: {}", payee.getId());
+    private final PayoutMemberRepository payoutMemberRepository;
+    private final PayoutRepository payoutRepository;
+
+    //PayoutMember가 생성되는 이벤트가 발행이 될 때 payout을 만든다.
+    public Payout createPayout(PayoutMemberDto payee) {
+        PayoutMember _payee = payoutMemberRepository.getReferenceById(payee.getId());
+        return payoutRepository.save(new Payout(_payee));
     }
 }
